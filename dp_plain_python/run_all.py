45b7f846d3286fa9f90137d468933e50b2139e6a
ymekesser <ymekesser@hotmail.com>
2023-05-14 13:03:19 +0800

    Define pipeline entry point

---
+++ dp_plain_python/run_all.py
@@ -0,0 +1,12 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+def main():
+    log.info(f'Data Pipeline started.')
+
+    # Extract
+    # Load
+    # Transform
+    # Analytics
