6846809b919ce6170ca2adba64cd3024df4a6cef
ymekesser <ymekesser@hotmail.com>
2023-05-27 17:11:24 +0800

    Use config and storage in load step

--- dp_plain_python/load/run_load.py
+++ dp_plain_python/load/run_load.py
@@ -1,27 +1,27 @@
 import json
 from pathlib import Path
 import pandas as pd
-from os import makedirs
 import logging
+from dp_plain_python.environment import config, file_storage
 
 log = logging.getLogger(__name__)
 
-# todo make configurable
-staging_path = "local_data\\staging"
-storage_path = "local_data\\storage"
-resale_flat_prices_filename = (
-    "resale-flat-prices-based-on-registration-date-from-jan-2017-onwards.csv"
-)
-mrt_stations_filename = "mrt_stations.xlsx"
+staging_path = config.get_location("Staging")
+storage_path = config.get_location("Storage")
+
+resale_flat_prices_filename = config.get_sourcefile_path("ResaleFlatPrices").name
+mrt_stations_filename = config.get_sourcefile_path("MrtStations").name
 mrt_geodata_filename = "mrt_geodata.json"
 mall_geodata_filename = "mall_geodata.json"
-address_geodata_filename = "address_geolocations.csv"
+address_geodata_filename = config.get_sourcefile_path("HdbAddressGeodata").name
+
+storage = file_storage.LocalFileStorage()
 
 
 def load_into_storage():
     log.info(f"Starting Loading Step.")
 
-    makedirs(storage_path, exist_ok=True)
+    storage.ensure_directory(storage_path)
 
     _load_resale_flat_prices()
     _load_mrt_stations()
@@ -33,9 +33,9 @@ def load_into_storage():
 def _load_resale_flat_prices():
     log.info(f"Loading {resale_flat_prices_filename} from staging into storage")
 
-    source = Path(staging_path) / resale_flat_prices_filename
+    source = staging_path / resale_flat_prices_filename
 
-    df = pd.read_csv(source)
+    df = storage.read_dataframe(source)
 
     _store_as_csv(df, "resale_flat_prices")
 
@@ -43,7 +43,7 @@ def _load_resale_flat_prices():
 def _load_mrt_stations():
     log.info(f"Loading {mrt_stations_filename} from staging into storage")
 
-    source = Path(staging_path) / mrt_stations_filename
+    source = staging_path / mrt_stations_filename
     df = pd.read_excel(source, sheet_name="Sheet1")
 
     _store_as_csv(df, "mrt_stations")
@@ -52,8 +52,8 @@ def _load_mrt_stations():
 def _load_mrt_geodata():
     log.info(f"Loading {mrt_geodata_filename} from staging into storage")
 
-    source = Path(staging_path) / mrt_geodata_filename
-    df = _load_json_dataframe(source)
+    source = staging_path / mrt_geodata_filename
+    df = _load_overpass_json_dataframe(source)
 
     _store_as_csv(df, "mrt_geodata")
 
@@ -61,8 +61,8 @@ def _load_mrt_geodata():
 def _load_mall_geodata():
     log.info(f"Loading {mall_geodata_filename} from staging into storage")
 
-    source = Path(staging_path) / mall_geodata_filename
-    df = _load_json_dataframe(source)
+    source = staging_path / mall_geodata_filename
+    df = _load_overpass_json_dataframe(source)
 
     _store_as_csv(df, "mall_geodata")
 
@@ -70,14 +70,14 @@ def _load_mall_geodata():
 def _load_address_geodata():
     log.info(f"Loading {address_geodata_filename} from staging into storage")
 
-    source = Path(staging_path) / address_geodata_filename
+    source = staging_path / address_geodata_filename
 
     df = pd.read_csv(source)
 
     _store_as_csv(df, "address_geodata")
 
 
-def _load_json_dataframe(source: Path):
+def _load_overpass_json_dataframe(source: Path):
     with source.open(encoding="utf-8") as f:
         data = json.load(f)
 
@@ -85,11 +85,6 @@ def _load_json_dataframe(source: Path):
     return pd.DataFrame.from_dict(data_dict, orient="columns")  # type: ignore
 
 
-def _store_as_parquet(df: pd.DataFrame, filename: str) -> None:
-    destination = Path(storage_path) / (filename + ".parquet")
-    df.to_parquet(destination)
-
-
 def _store_as_csv(df: pd.DataFrame, filename: str) -> None:
-    destination = Path(storage_path) / (filename + ".csv")
-    df.to_csv(destination)
+    destination = storage_path / (filename + ".csv")
+    storage.write_dataframe(df, destination)
