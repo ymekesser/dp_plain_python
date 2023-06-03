9038a45bb5b4ecd44b729ee228ad3019ad7c18dc
ymekesser <ymekesser@hotmail.com>
2023-06-03 14:00:11 +0800

    Add API endpoint

---
+++ dp_plain_python/api.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from dp_plain_python.run_all import main
+from dp_plain_python.environment import config
+
+app = FastAPI()
+
+
+@app.post("/run")
+async def run():
+    main()
+
+    return {"message": "Data pipeline completed successfully!"}
